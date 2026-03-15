a3a595a0aa50adc0084e8aafb6f00cfc7da08054
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-03-15 22:20:49 +0300

    Refactor restaurant socket event handling

--- backendNUMNOW/sockets/restaurant.socket.js
+++ backendNUMNOW/sockets/restaurant.socket.js
@@ -1,6 +1,6 @@
 const Order = require("../models/Order");
 const Driver = require("../models/Driver");
-const Restaurant = require("../models/Restaurant");
+const Restaurant = require("../models/restaurant");
 const { findAndNotifyDrivers } = require("./services/order.service");
 
 module.exports = (io, restaurantNS) => {
