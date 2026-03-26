c1ea92de686208e62d431465e4b0f80e28b30ddb
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-03-26 16:07:18 +0300

    Delete frontend-nomnow - Admin directory

--- frontend-nomnow - Admin/src/socket/adminSocket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
-
-export const adminSocket = io(`${SOCKET_URL}/admin`, {
-  withCredentials: true,
-  autoConnect: false, // مهم جداً
-});
