1c53fe4c26d88d631d5eee044f0fd1265be43f7e
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-03-27 04:12:13 +0300

    Delete backendNUMNOW directory

--- backendNUMNOW/sockets/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ io, adminNS, driverNS, restaurantNS, userNS }) => {
-  require("./user.socket")(io, userNS);
-  require("./restaurant.socket")(io, restaurantNS);
-  require("./driver.socket")(io, driverNS, adminNS);
-};
