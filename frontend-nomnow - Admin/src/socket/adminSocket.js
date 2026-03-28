5a3f24f63e070a099885ff6f415e7a0fee4fbfb5
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-03-28 04:12:20 +0300

    Add files via upload

---
+++ frontend-nomnow - Admin/src/socket/adminSocket.js
@@ -0,0 +1,9 @@
+// === ADMIN ===
+import { io } from "socket.io-client";
+
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
+
+export const adminSocket = io(`${SOCKET_URL}/admin`, {
+  withCredentials: true,
+  autoConnect: false, // مهم جداً
+});
