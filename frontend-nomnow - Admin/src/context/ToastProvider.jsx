21d17a787caf3bb9d97b027192a7768f3093fb31
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2026-04-19 17:27:44 +0400

    Add files via upload

---
+++ frontend-nomnow - Admin/src/context/ToastProvider.jsx
@@ -0,0 +1,17 @@
+// === ADMIN ===
+import { Toaster } from "react-hot-toast";
+import { createPortal } from "react-dom";
+
+const ToastProvider = () => {
+  return createPortal(
+    <Toaster
+      position="top-right"
+      containerStyle={{
+        zIndex: 2147483647,
+      }}
+    />,
+    document.body,
+  );
+};
+
+export default ToastProvider;
